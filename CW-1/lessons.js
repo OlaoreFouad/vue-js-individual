e5de55fee9673ce238aa58c634df35f093f91dbb
Olaore Fouad <olaorefouad565@gmail.com>
2021-03-10 17:35:53 +0400

    feature: worked on setting up the Vue plugin and outsourced the lessons details to another file, importing the lessons by adding the script to the page

---
+++ CW-1/lessons.js
@@ -0,0 +1,72 @@
+const lessons = [
+    {
+        name: 'Biology',
+        location: 'Brussels',
+        price: 1000,
+        spaces: 5,
+        image: './images/biology.png'
+    },
+    {
+        name: 'Maths',
+        location: 'Montenegro',
+        price: 270,
+        spaces: 5,
+        image: './images/maths.jpg'
+    },
+    {
+        name: 'Physics',
+        location: 'Paris',
+        price: 1500,
+        spaces: 5,
+        image: './images/physics.png'
+    },
+    {
+        name: 'Chemistry',
+        location: 'Cairo',
+        price: 400,
+        spaces: 5,
+        image: './images/chemistry.jpg'
+    },
+    {
+        name: 'Programming',
+        location: 'Tokyo',
+        price: 250,
+        spaces: 5,
+        image: './images/programming.jpg'
+    },
+    {
+        name: 'Databases',
+        location: 'Nairobi',
+        price: 1400,
+        spaces: 5,
+        image: './images/databases.png'
+    },
+    {
+        name: 'Economics',
+        location: 'Stockholm',
+        price: 560,
+        spaces: 5,
+        image: './images/economics.jpg'
+    },
+    {
+        name: 'Advanced Mathematics',
+        location: 'Denver',
+        price: 1200,
+        spaces: 5,
+        image: './images/adv_maths.jpg'
+    },
+    {
+        name: 'Data Processing',
+        location: 'Rio',
+        price: 410,
+        spaces: 5,
+        image: './images/data-processing.jpg'
+    },
+    {
+        name: 'Anatomy',
+        location: 'Carlifonia',
+        price: 700,
+        spaces: 5,
+        image: './images/anatomy.jpg'
+    },
+]
